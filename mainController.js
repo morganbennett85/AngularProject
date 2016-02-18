3af8b8c70a7c14042f82e319da70c337ff05fc67
Morgan Bennett <morganmargaret85@gmail.com>
2016-02-18 07:58:16 -0500

    Update mainController.js

--- mainController.js
+++ mainController.js
@@ -1,88 +1,68 @@
 app.controller('MainController', ['$scope', function($scope) {
 	
-	$scope.colors = [
-		{
-			name: "Red Value",
-			value: 255
+	
+	$scope.colors = {
+			redValue: 255,
+			blueValue: 255,
+			greenValue: 255
 		},
-		{
-			name: "Blue Value",
-			value: 255
-		},
-		{
-			name: "Green Value",
-			value: 255
-		
-		}],
 	$scope.increaseRed = function() {
 		
-		$scope.colors[0].value += 5;
-		if($scope.colors[0].value > 255)
+		$scope.colors.redValue += 5;
+		if($scope.colors.redValue > 255)
 		{
-			$scope.colors[0].value = 255;
+			$scope.colors.redValue = 255;
 		}
 	};
 	$scope.increaseBlue = function() {
 		
-		$scope.colors[1].value += 5;
-		if($scope.colors[1].value > 255)
+		$scope.colors.blueValue += 5;
+		if($scope.colors.blueValue > 255)
 		{
-			$scope.colors[1].value = 255;
+			$scope.colors.blueValue = 255;
 		}
 	};
 	$scope.increaseGreen = function() {
 		
-		$scope.colors[2].value += 5;
-		if($scope.colors[2].value > 255)
+		$scope.colors.greenValue += 5;
+		if($scope.colors.greenValue > 255)
 		{
-			$scope.colors[2].value = 255;
+			$scope.colors.greenValue = 255;
 		}
 	};
 	$scope.decreaseRed = function() {
 		
-		$scope.colors[0].value -= 5;
-		if($scope.colors[0].value < 0){
-			$scope.colors[0].value = 0;
+		$scope.colors.redValue -= 5;
+		if($scope.colors.redValue < 0){
+			$scope.colors.redValue = 0;
 		}
 	};
 	$scope.decreaseBlue = function() {
 		
-		$scope.colors[1].value -= 5;
-		if($scope.colors[1].value < 0){
-			$scope.colors[1].value = 0;
+		$scope.colors.blueValue -= 5;
+		if($scope.colors.blueValue < 0){
+			$scope.colors.blueValue = 0;
 		}
 	};
 	$scope.decreaseGreen = function() {
 		
-		$scope.colors[2].value -= 5;
-		if($scope.colors[2].value < 0){
-			$scope.colors[2].value = 0;
+		$scope.colors.greenValue -= 5;
+		if($scope.colors.greenValue < 0){
+			$scope.colors.greenValue = 0;
 		}
 	};
-	/*$scope.updateRed = function(value) {
-		
-		$scope.colors[0].value = value;
-	};
-	$scope.updateGreen = function(value) {
-		
-		$scope.colors[2].value = value;
-	};
-	$scope.updateBlue = function(value) {
-		
-		$scope.colors[1].value = value;
-	};*/
 	$scope.resetRed = function() {
-		$scope.colors[0].value = 255;
+		$scope.colors.redValue = 255;
 	};
 	$scope.resetBlue = function() {
-		$scope.colors[1].value = 255;
+		$scope.colors.blueValue = 255;
 	};
 	$scope.resetGreen = function() {
-		$scope.colors[2].value = 255;
+		$scope.colors.greenValue = 255;
 	};
 	$scope.resetAll = function() {
-		$scope.colors[0].value = 255;
-		$scope.colors[1].value = 255;
-		$scope.colors[2].value = 255;
+		$scope.colors.redValue = 255;
+		$scope.colors.blueValue = 255;
+		$scope.colors.greenValue = 255;
 	};
 }]);
